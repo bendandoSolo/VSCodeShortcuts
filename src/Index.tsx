cc571c08e36a6b81af54aec5e1df62eb8a606832
Ben Roberts <bendando@hotmail.co.uk>
2025-04-06 12:36:52 +0100

    added MUI

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ReadMe } from './sections/ReadMe.tsx'
 
 function Index() {
   const [count, setCount] = useState(0)
@@ -17,7 +18,8 @@ function Index() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      {/*  TODO: Add a ReadMe and add the extension to view MD files, perhaps project for the same */}
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -31,19 +33,10 @@ function Index() {
       <p> 
         Testing the development using vite , quick loading, and react router, 
 
-      </p>
+      </p> */}
+      <ReadMe />
       <p>
-        Pages: 
-        1 navigation, primary importance is learing swift navigation
-        2. code editing
-        3 other 
-        4 toDo list
-        - 1 set up basic app showing all the useful features we need to remember on one page
-        - 3 set up nav bar
-        - 2 set up accordion/ use of mui
-        - set up pages fore each of 3 above
-        - link with google drive
-        - make game out of remembering the features
+       
       </p>
     </>
   )
--- src/sections/ReadMe.tsx
+++ src/sections/ReadMe.tsx
@@ -1,10 +1,31 @@
-export const ReadMe = <>
-    <h1>ReadMe</h1>
-    <p>
-        This is a simple React app that allows you to create and manage notes. You can add, edit, and delete notes, and they will be saved in local storage.
-    </p>
-    <p>
-        The app uses TypeScript, React, and Tailwind CSS for styling. It is designed to be simple and easy to use.
-    </p>
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
 
-</>
+interface IReadMeProps {
+}
+
+export const ReadMe: React.FunctionComponent<IReadMeProps> = (props) => {
+  return <>
+    <h1>Read Me</h1>
+
+    <h2>Todo</h2>
+    <h4>Priority 1 is to copy over the old data so we don't have to reference the old projects and can delete it</h4>
+
+    <Stack spacing={2} direction="column" alignItems="flex-centre">
+        <h4>Pages and basic setup</h4>
+        <div>1. Navigation</div>
+        <div>2. Code Editing</div>
+        <div>3. Other</div>
+        <div>4. Learn TypeScript Code Snippet</div>
+        <div>5. ToDo List</div>
+        <div>5 toDo list advanced features</div>
+        <div>- 1 set up basic app showing all the useful features we need to remember on one page</div>
+        <div> - 3 set up nav bar</div>
+        <div>- 2 set up accordion/ use of mui</div>
+        <div>- set up pages fore each of 3 above</div>
+        <div>- link with google drive</div>
+        <div>- make game out of remembering the features</div>
+    </Stack>
+  </>;
+};
+
