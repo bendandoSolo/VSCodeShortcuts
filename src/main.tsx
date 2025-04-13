292995a40dd937150ffba94106f58a011b7d5add
Ben Roberts <bendando@hotmail.co.uk>
2025-04-13 16:39:25 +0100

    using super basic react router dom in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import Index from './Index.tsx'
+import { ReadMe } from './sections/ReadMe.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   <StrictMode>
-    <Index />
+  <Routes>
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/" element={<Index />} />
+          <Route path="/readMe" element={<ReadMe />} />
+        {/* </Route> */}
+      </Routes>
   </StrictMode>
   </BrowserRouter>,
 )
